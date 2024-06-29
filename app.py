c43f1173d8233dd360151dc795339196e6da98a7
Dao Nguyen <dao1703songngu@gmail.com>
2024-06-29 11:20:44 +0700

    resolve error H10

--- app.py
+++ app.py
@@ -178,7 +178,7 @@ def create_app(test_config=None):
     }), error.status_code
   return app
 
-APP = create_app()
+app = create_app()
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    APP.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
