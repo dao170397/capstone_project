29f1382589ada44111799e77834de33c78c7d6a8
Dao Nguyen <dao1703songngu@gmail.com>
2024-06-29 10:04:14 +0700

    update database url

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 
 #!/bin/bash
-export DATABASE_URL="postgresql://postgres:sys@localhost:5432/capstone"
+export DATABASE_URL="postgres://u44hrbbhcjkkg3:pa24c80194b2a19e3afce3003dae922e77b0cc66236ae6a0712d05f2f7a4ef60d@c5p86clmevrg5s.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d55sbi02egqgpv"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
